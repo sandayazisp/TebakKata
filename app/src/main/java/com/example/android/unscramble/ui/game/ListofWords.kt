5ea8fdfce1d6164f1419c232f2eda41faba4238b
sanday <sandayazisp12@gmail.com>
2022-11-11 11:44:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 10
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf(
+        "aceh",
+        "medan",
+        "palembang",
+        "padang",
+        "bengkulu",
+        "pekanbaru",
+        "jambi",
+        "pontianak",
+        "samarinda",
+        "banjarmasin",
+        "palangkaraya",
+        "serang",
+        "jakarta",
+        "bandung",
+        "semarang",
+        "yogyakarta",
+        "surabaya",
+        "bali",
+        "kupang",
+        "mataram",
+        "gorontalo",
+        "palu",
+        "manado",
+        "kendari",
+        "makasar",
+        "ternate",
+        "ambon",
+        "jayapura")
